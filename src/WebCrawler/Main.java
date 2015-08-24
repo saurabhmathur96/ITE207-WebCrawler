448c558839e4063c3861a1498de3e4a19caeaec9
saurabhmathur96 <saurabhmathur96@gmail.com>
2015-08-24 15:28:33 +0530

    create package WebCrawler

--- src/WebCrawler/Main.java
+++ src/WebCrawler/Main.java
@@ -1,8 +1,12 @@
+package WebCrawler;
+
 /**
  * Created by saurabh on 24/8/15.
  */
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("Crawler");
     }
 }
+
